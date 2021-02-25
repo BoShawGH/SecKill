7bd4592ff7bcec2b296db5462190bb0343bcf2ce
yongbo.xiao <yongbo.xiao@outlook.com>
2021-02-26 00:24:09 +0800

    add product's func

--- src/main/java/com/yates/controller/ProductController.java
+++ src/main/java/com/yates/controller/ProductController.java
@@ -4,11 +4,16 @@ import com.yates.entity.Product;
 import com.yates.service.ProductService;
 import com.yates.vo.CustomVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -16,6 +21,7 @@ import java.util.List;
 public class ProductController {
     @Autowired
     private ProductService productService;
+    private DateTimeFormatter dateTimeFormatter= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @RequestMapping(value = "/toapply")
     public String toApply(){
@@ -33,6 +39,58 @@ public class ProductController {
         List<Product> products = productService.listProducts(customVo);
         request.setAttribute("products", products);
         return "/product/list";
-
     }
+
+    @RequestMapping(value = "/querybyid")
+    public String queryProductById(HttpServletRequest request, int id){
+        Product product = productService.queryProductById(id);
+        request.setAttribute("product", product);
+        return "/product/view";
+    }
+
+    @RequestMapping(value = "/delete")
+    public String deleteProductById(int id){
+        productService.deleteProductById(id);
+        return "redirect:/product/listproducts";
+    }
+
+    @RequestMapping(value = "/toupdate")
+    public String toUpdateById(HttpServletRequest request, int id){
+        Product product = productService.queryProductById(id);
+        if(product.getApplyDateString() == null || product.getAuditDateString() == null){
+            product.setApplyDateString(product.getApplyDate().format(dateTimeFormatter));
+            product.setAuditDateString(product.getAuditDate().format(dateTimeFormatter));
+        }
+        String startTime = product.getStartTime().format(dateTimeFormatter);
+        String endTime = product.getEndTime().format(dateTimeFormatter);
+        request.setAttribute("product", product);
+        request.setAttribute("startTime", startTime);
+        request.setAttribute("endTime", endTime);
+        return "/product/update";
+    }
+
+    @RequestMapping(value = "/update")
+    public String updateProductById(HttpServletRequest request, Product product, String startTimeString, String endTimeString){
+//        if(!startTimeString.equals(product.getStartTimeString())){
+//            product.setStartTimeString(startTimeString);
+//            product.setStartTime(LocalDateTime.parse(startTimeString, dateTimeFormatter));
+//        }
+//        if(!endTimeString.equals(product.getEndTimeString())){
+//            product.setEndTimeString(endTimeString);
+//            product.setEndTime(LocalDateTime.parse(endTimeString, dateTimeFormatter));
+//        }
+        product.setApplyDate(LocalDateTime.parse(product.getApplyDateString(), dateTimeFormatter));
+        product.setStartTime(LocalDateTime.parse(product.getStartTimeString(), dateTimeFormatter));
+        product.setEndTime(LocalDateTime.parse(product.getEndTimeString(), dateTimeFormatter));
+        product.setAuditDate(LocalDateTime.parse(product.getAuditDateString(), dateTimeFormatter));
+        productService.updateProduct(product);
+        return "redirect:/product/listproducts";
+    }
+
+    @RequestMapping(value = "/updatestate")
+    public String updateProductState(int id, int state){
+        productService.updateProductState(id, state);
+        return "redirect:/product/listproducts";
+    }
+
 }
--- src/main/java/com/yates/service/ProductService.java
+++ src/main/java/com/yates/service/ProductService.java
@@ -3,6 +3,7 @@ package com.yates.service;
 import com.yates.dao.IProductDao;
 import com.yates.entity.Product;
 import com.yates.vo.CustomVo;
+import com.yates.vo.ProductVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +15,18 @@ import java.util.Date;
 import java.util.List;
 
 @Service
-public class ProductService implements IProductDao{
+public class ProductService {
     @Autowired
     public IProductDao iProductDao;
     public DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     /**
      * 申请秒杀商品
      * @param product
      */
     public void applyProduct(Product product){
         product.setApplyDate(LocalDateTime.now());
+        product.setApplyDateString(product.getApplyDate().format(dateTimeFormatter));
         product.setAuditState(0);
         try {
             product.setStartTime(LocalDateTime.parse(product.getStartTimeString(), dateTimeFormatter));
@@ -42,5 +45,46 @@ public class ProductService implements IProductDao{
      */
     public List<Product> listProducts(CustomVo customVo){
         return iProductDao.listProducts(customVo);
+    }
+
+    /**
+     * 通过id查询商品
+     * @param id
+     * @return
+     */
+    public Product queryProductById(int id){
+        return iProductDao.queryProductById(id);
+    }
+
+    /**
+     * 删除指定id商品
+     * @param id
+     */
+    public void deleteProductById(int id){
+        iProductDao.deleteProductById(id);
+    }
+
+    /**
+     * 更新商品
+     * @param product
+     */
+    public void updateProduct(Product product){
+        iProductDao.updateProduct(product);
+    }
+
+    /**
+     * 更新产品状态
+     * @param id
+     * @param state
+     */
+    public void updateProductState(int id, int state){
+        CustomVo customVo = new CustomVo();
+        ProductVo productVo = new ProductVo();
+        productVo.setAuditState(state);
+        productVo.setId(id);
+        productVo.setAuditDate(LocalDateTime.now());
+        productVo.setAuditDateString(productVo.getAuditDate().format(dateTimeFormatter));
+        customVo.setProductVo(productVo);
+        iProductDao.updateProductState(customVo);
     }
 }
